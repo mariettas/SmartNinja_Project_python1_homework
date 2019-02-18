40a40f43ceff06554ad2e6f1bdbd0f98cf188370
Marietta Sauerzapf <mariettasauerzapf@gmx.at>
2019-02-18 21:02:45 +0100

    commit secret_number

---
+++ secret_number.py
@@ -0,0 +1,9 @@
+secret= 9
+
+guess= int(input("What is the secret number? Enter a number between 1 and 9."))
+
+if guess == secret:
+    print("Winner!")
+else:
+    print("Sorry. Better luck next time.")
+
