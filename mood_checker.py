2a4397b6863c408ad5ac75a885faa2c70a35eaa6
Marietta Sauerzapf <mariettasauerzapf@gmx.at>
2019-02-18 20:47:44 +0100

    commit mood_checker

---
+++ mood_checker.py
@@ -0,0 +1,15 @@
+mood= input ("What mood are you in?")
+
+if mood == "happy":
+    print("It is great to see you happy!")
+elif mood == "nervous":
+    print("Take a deep breath 3 times.")
+elif mood == "sad":
+    print("Cheer up!")
+elif mood == "excited":
+    print("Glad to see you are excited!")
+elif mood == "relaxed":
+    print ("How come? Your exam is tomorrow.")
+else:
+    print ("Mood not recognized.")
+
